e17ae6ec4c5f2f5354c5fdd482285bc776dbdfbc
acolburn <acolburn3@gmail.com>
2022-10-30 16:17:31 -0700

    changed wine type list

--- wine_type_list.py
+++ wine_type_list.py
@@ -1,10 +1,10 @@
 wine_types = {
-"bold red" : ["malbec","syrah/shiraz","mourvedre","pinotage","petite sirah","cabernet sauvignon","bordeaux blend","meritage"],
-"medium red": ["merlot","sangiovese","zinfandel","cabernet franc","tempranillo","nebbiolo","barberra","cotes du rhone blend"],
+"bold red" : ["malbec","syrah/shiraz","mourvedre","pinotage","petite syrah","cabernet sauvignon","bordeaux blend","meritage"],
+"medium red": ["merlot","sangiovese","chianti", "zinfandel","cabernet franc","tempranillo","nebbiolo","barberra","cotes du rhone blend"],
 "light red" : ["pinot noir", "grenache", "gamay", "St. Laurent","carignon"],
 "rose" : ["provencal rose", "white zinfandel", "Loire valley rose","pinot noir rose","syrah rose","garnacha rosado","bandol rose","tempranillo rose"],
 "rich white" : ["chardonnay", "semillon", "viognier", "marsanne", "roussane"],
-"light white" : ["sauvignon blanc", "albarino", "pinot blanc", "vermentino", "gergenega", "treggiano", "pinot grigio"],
+"light white" : ["sauvignon blanc", "dry riesling", "albariño", "pinot blanc", "vermentino", "gergenega", "treggiano", "pinot grigio"],
 "sparkling" : ["champagne", "prosecco", "cremant", "cava", "sparkling wine", "sparkling rose"],
 "sweet white" : ["moscato", "riesling", "chenin blanc", "gewurztraminer", "late harvest whites"],
 "dessert" : ["port", "sherry", "madeira", "vin santo", "muscat"]
